2eee4361fb3bf1c8332eadf923cc57142b0558d5
Zizzixsec <zizzixsec@gmail.com>
2024-04-04 13:59:15 -0500

    add basics of nyx.obj.rotate

---
+++ obj/rotate.cs
@@ -0,0 +1,25 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+// using VRC.SDKBase;
+// using VRC.Udon;
+
+namespace nyx.obj {
+    public class rotate : UdonSharpBehaviour {
+
+        public GameObject gameObj;
+        private bool spin = false;
+
+        public void Update() {
+            if(spin) {
+                gameObj.transform.Rotate(Vector3.up, 90f * Time.deltaTime);
+                gameObj.transform.Rotate(Vector3.right, 90f * Time.deltaTime);
+            }
+        }
+
+        public override void Interact() {
+            spin = !spin;
+        }
+    }
+}
+
